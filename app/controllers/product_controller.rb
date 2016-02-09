f5e25a740bab40a43ec9fb2d91a50d3f6d4fbd10
MalarCB <malarvizhy.soupramanien@careerbuilder.com>
2016-02-09 23:47:00 +0100

    change to seo name

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -15,14 +15,14 @@ class ProductController < ApplicationController
     @products = Product.find_by_sql("
              SELECT c.id, c.title, m.name as category_name, m.seo_name as seoname, c.* FROM products c
                INNER JOIN sub_categories u ON u.id = c.sub_category_id
-                 INNER JOIN categories m ON m.id = u.category_id where c.status = 't' and lower(category_name) = '#{params[:category]}' ").group_by(&:category_name)
+                 INNER JOIN categories m ON m.id = u.category_id where c.status = 't' and lower(m.seo_name) = '#{params[:category]}' ").group_by(&:category_name)
   end
 
   def shop_by_sub_category
     @products = Product.find_by_sql("
              SELECT c.id, c.title, u.seo_name as category_name, u.seo_name as seoname, c.* FROM products c
                INNER JOIN sub_categories u ON u.id = c.sub_category_id
-                 where c.status = 't' and lower(category_name) = '#{params[:sub_category]}' ").group_by(&:category_name)
+                 where c.status = 't' and lower(u.seo_name) = '#{params[:sub_category]}' ").group_by(&:category_name)
 
   end
 
