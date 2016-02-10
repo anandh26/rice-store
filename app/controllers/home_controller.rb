fb222c4dd8e619c19e430309a85afce7c17ad244
MalarCB <malarvizhy.soupramanien@careerbuilder.com>
2016-02-10 22:14:25 +0100

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    join_query = Product.select('products.*, sub_categories.*').joins(:sub_category).where("products.status = 't'").order("CREATED_AT DESC").all
-    @products = join_query.group_by(&:category_name)
-    p @products
-    p '===='
+    # join_query = Product.select('products.*, sub_categories.*').joins(:sub_category).where("products.status = 't'").order("CREATED_AT DESC").all
+    # @products = join_query.group_by(&:category_name)
+    # p @products
+    # p '===='
   end
 end
